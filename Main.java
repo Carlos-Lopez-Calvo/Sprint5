05f29804dc9d5a7cda86dae2b336c98a0ea3bc53
Carlos López Calvo <clopezcalvo@cifpfbmoll.eu>
2024-10-31 17:16:08 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,48 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int edad = 4;
+        int ciclo = 0;
+        int curso = 0;
+
+        if (edad <3) {
+            System.out.println("Es muy pequeño!!!");
+        } else if (edad >= 3 && edad < 6) {
+            ciclo = 0;
+        } else if (edad == 6) {
+            curso = 1;
+            ciclo = 1;
+        } else if (edad == 7) {
+            curso = 2;
+            ciclo = 1;
+        } else if (edad == 8) {
+            curso = 3;
+            ciclo = 2;
+        } else if (edad == 9) {
+            curso = 4;
+            ciclo = 2;
+        } else if (edad == 10) {
+            curso = 5;
+            ciclo = 3;
+        } else if (edad == 11) {
+            curso = 6;
+            ciclo = 3;
+        } else {
+            System.out.println("Es muy grande!!");
+        }
+
+        if (ciclo == 0) {
+            System.out.println("Es de Educacion infantil");
+        } else if (ciclo == 1) {
+            System.out.println("Es de Educacion primaria ciclo inicial, el curso es: "+ curso);
+        } else if (ciclo == 2) {
+            System.out.println("Es de Educacion primaria ciclo medio, el curso es: "+ curso);
+        } else if (ciclo == 3) {
+            System.out.println("Es de Educacion primaria ciclo superior, el curso es : "+ curso);
+        }
+
+
+
+    }
+}
